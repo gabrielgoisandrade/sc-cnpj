9aaf23ccbcf42bc22d917ecdc3c1941f234cda8d
gabrielgoisandrade <gabriel.gois.andrade14@gmail.com>
2026-08-28 17:21:31 -0300

    wip

--- app/src/app/__init__.py
+++ app/src/app/__init__.py
@@ -1,67 +1,140 @@
-from time import sleep
+import csv
+import logging
+from pathlib import Path
+from time import perf_counter, sleep
 
+from openpyxl import load_workbook
+from openpyxl.utils import column_index_from_string
+from selenium.common.exceptions import TimeoutException, WebDriverException
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
+logging.basicConfig(
+    filename="app.log",
+    filemode="a",
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=logging.INFO,
+)
 
-def find_value_by_cnpj(cnpjs: list[str]) -> list[dict[str, str]]:
+log = logging.getLogger("log")
+
+
+def get_driver():
     try:
         options = Options()
         options.add_argument("--headless=new")
-        driver = Chrome(options)
 
-        driver.get("https://appasp.sefaz.go.gov.br/Sintegra/Consulta/default.html")
+        service = Service(executable_path=Path("src/driver/chromedriver.exe").resolve())
+
+        driver = Chrome(options, service)
+        driver.set_page_load_timeout(10)
+
+        log.info("Driver found")
+        return driver
+    except FileNotFoundError as e:
+        log.exception(msg=e)
+        raise e from e
+
+
+def find_value_by_cnpj(cnpjs: list[str]) -> list[str]:
+    start = perf_counter()
+
+    log.info(f"Starting search {len(cnpjs)} CNPJs")
+    driver = get_driver()
+
+    index_url = "https://appasp.sefaz.go.gov.br/Sintegra/Consulta/default.html"
+    result_page_url = "https://appasp.sefaz.go.gov.br/Sintegra/Consulta/consultar.asp"
+
+    results = []
+
+    try:
+        driver.get(index_url)
+
         checkbox = driver.find_element("id", "rTipoDocCNPJ")
-        checkbox.click()
-
         input = driver.find_element("id", "tCNPJ")
-
         confirm = driver.find_element("name", "btCGC")
 
-        results = []
+        checkbox.click()
 
-        for cnpj in cnpjs:
-            input.click()
+        for index, cnpj in enumerate(cnpjs):
+            sleep(1.5)
+            log.info(f"Searching {index + 1}/{len(cnpjs)}")
+
+            input.clear()
             input.send_keys(cnpj)
-
-            sleep(1)
 
             confirm.click()
 
-            sleep(1)
+            sleep(1.5)
 
-            span = driver.find_element(
-                By.XPATH, "//span[text()='Regime de Apuração:']/following-sibling::*[1]"
+            error_tooltip = driver.find_elements(
+                By.CLASS_NAME,
+                "zion_rich_validation_box",
             )
 
-            results.append({"cnpj": cnpj, "situation": span.text})
+            if len(error_tooltip):
+                log.error(f"{cnpj} is invalid")
 
-            driver.back()
+                results.append("CNPJ inválido")
+                input.clear()
+
+                continue
 
             sleep(1.5)
 
+            span = driver.find_elements(
+                By.XPATH, "//span[text()='Regime de Apuração:']/following-sibling::*[1]"
+            )
+
+            if result_page_url == driver.current_url and not len(span):
+                log.info(f"No results found for {cnpj}")
+
+                results.append(" ")
+                driver.back()
+
+                continue
+
+            log.info(f"Results found: {len(results)} of {len(cnpjs)}")
+
+            results.append(span[0].text)
+
+            driver.back()
+
+        end = perf_counter()
+
         return results
-    except ValueError as e:
+    except (WebDriverException, TimeoutException) as e:
+        log.exception(msg=e)
         raise e from e
     finally:
+        log.info(f"Finished at {(end - start):.4f}")
         driver.quit()
 
 
 def main() -> None:
     try:
+        file = r"C:\Users\gabriel.andrade\Documents\cnpjs_export_with_situation.xlsx"
+        wb = load_workbook(file)
+        sheet = wb.active
+
         cnpjs = [
-            "02757995000165",
-            "00294421000172",
-            "19258309000104",
-            "12561303000162",
-            "47387347000100",
+            str(value)
+            for col in sheet.iter_cols(
+                min_row=2, max_row=1000, max_col=1, min_col=1, values_only=True
+            )
+            for value in col
         ]
 
-        values = find_value_by_cnpj(cnpjs)
+        results = find_value_by_cnpj(cnpjs)
+        situation_column = column_index_from_string("E")
+        sheet.cell(column=situation_column, row=1, value="Situação")
 
-        with open("../output.txt", "w+") as f:
-            [f.write(f"{value['cnpj']} - {value['situation']}\n") for value in values]
+        for index, result in enumerate(results, start=2):
+            sheet.cell(column=situation_column, row=index, value=result)
 
-    except ValueError as e:
-        print(e)
+        wb.save(r"C:\Users\gabriel.andrade\Documents\cnpjs_export_with_situation.xlsx")
+    except (WebDriverException, UnboundLocalError) as e:
+        log.exception(msg=e)
