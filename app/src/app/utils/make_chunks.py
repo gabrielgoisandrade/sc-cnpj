f2cf316103620263a461ff4c8d77be58b261e72c
gabrielgoisandrade <gabriel.gois.andrade14@gmail.com>
2026-09-02 16:49:44 -0300

    chore: tipagem ajustada

--- app/src/app/utils/make_chunks.py
+++ app/src/app/utils/make_chunks.py
@@ -1,10 +1,13 @@
 from typing import Any
 
 from app.logger import log
+from app.models import Record
 
 
 def make_chunks(iterable: list[Any], chunk_size=1_000):
-    chunks = [iterable[i : i + chunk_size] for i in range(0, len(iterable), chunk_size)]
+    chunks: list[list[Record]] = [
+        iterable[i : i + chunk_size] for i in range(0, len(iterable), chunk_size)
+    ]
 
     log.info(f"Created a chunk with {chunk_size} items each")
     return chunks
